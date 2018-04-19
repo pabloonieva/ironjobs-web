d8dec089ef803f0305ce1c3d645cf9a88a8e5542
pabloonieva <pablo@ironhack.com>
2018-04-19 21:22:25 +0200

    feat: video added in home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'offers', component: ListOffersComponent },
